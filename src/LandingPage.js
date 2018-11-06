66be0afdd69e81663fcefa10ea103c5e4a7b27e1
yukims19 <yukims19@gmail.com>
2018-11-05 22:44:36 -0800

    Initial commit for landing page

---
+++ src/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="landing-page">
+        <div id="landing-page-filter" />
+        <h1>Hi. This is Yuki.</h1>
+        <h1>I love Design and Coding</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
