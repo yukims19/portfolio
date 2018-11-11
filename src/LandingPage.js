f768a9c987dab63959c3ccdc56ce738386fdaa6a
yukims19 <yukims19@gmail.com>
2018-11-11 13:25:35 -0800

    Use section

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 class LandingPage extends Component {
   render() {
     return (
-      <div className="landing-page">
+      <section className="landing-page">
         <div id="landing-page-filter" />
         <h1>Hi. This is Yuki.</h1>
         <h1>I love Design and Coding</h1>
-      </div>
+      </section>
     );
   }
 }
