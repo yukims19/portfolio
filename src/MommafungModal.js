3b96f63ae955f35f126d28caf0c02afe41f068e1
yukims19 <yukims19@gmail.com>
2019-01-14 14:31:14 -0800

    Fix grammer

--- src/MommafungModal.js
+++ src/MommafungModal.js
@@ -17,21 +17,19 @@ const MommafungModal = () => {
         <Row>
           <Col span={16} offset={4}>
             <p>
-              This unique street-food vendor, MommaFung, that harnesses the
-              essence of unicorn food needs to be discoverable by all the
-              instagram food influencers and picky eaters. We tell you why
-              Mommafung would be your first choice and where you can catch them.
-              <br />
+              This unique street food vendor that harnesses the essence of
+              unicorn food needs to be discoverable on the internet for all the
+              instagram food influencers and local foodies. We tell you why
+              MommaFung would be your first choice and where you can catch them.
             </p>
           </Col>
         </Row>
         <a
           href="http://yoshili.com/Moma_Fungs.github.io/index.html"
           target="_blank"
+          className="ant-btn ant-btn-primary ant-btn-lg overview-btn"
         >
-          <Button type="primary" className="overview-btn">
-            Open the Website
-          </Button>
+          Visit Website
         </a>
       </section>
 
@@ -46,27 +44,34 @@ const MommafungModal = () => {
         <Row className="section-content">
           <Col span={20} offset={2}>
             <p>
-              Katie started MommaFung, the steamed bun vendor business, because
-              she loved her mom’s buns and thought it was so tasty that it
-              should be shared with others. While Starbucks Unicorn drink
-              marketing campaign was causing a huge fever of rainbow food all
-              over the world, she decided to take advantage of that wave and
-              create a unique “unicorn” steamed bun business.
+              Katie started the steamed bun vendor business because she loved
+              her mom’s buns and thought that they are so tasty that should be
+              shared with others. She came up with the idea of selling the buns
+              which happened to coincide with the Starbucks Unicorn drink
+              marketing campaign which she used to her advantage in creating a
+              unique “unicorn” steamed bun that people loved. The food
+              vendor/truck emergence became hugely popular because of a number
+              of reasons. Eating evolved from just a necessity to become a
+              social activity that people engage in, and food vendors are the
+              perfect facilitators of that.
             </p>
             <p>
-              Their business brand is <strong>Cute</strong>,
+              MommaFung's business brand is <strong>Cute</strong>,
               <strong> Modest</strong>, and <strong>Thankful</strong>. Having a
-              bun in mommafung is not only about making your stomach full, but
-              more importantly, the experience of enjoying the look and the
-              taste of the food, witnessing the process the bun is made one by
-              one by hand, and sharing this excitement with your people.
+              bun in MommaFung is not only about filling your stomach, but more
+              importantly, about the experience of enjoying the aesthetics of
+              the food, witnessing the making process of the buns, and sharing
+              the excitement with other people.
             </p>
             <p>
               <i>
                 {" "}
-                Our detailed meeting summary and analysis can be accessed
-                through
-                <a href="https://docs.google.com/document/d/1LF2dC9u6YSdTlyFsY1Edwpx7lOZJE34WIr-CkgPQaKg/edit?usp=sharing">
+                More details about the client meeting and analysis can be
+                accessed through
+                <a
+                  href="https://docs.google.com/document/d/1LF2dC9u6YSdTlyFsY1Edwpx7lOZJE34WIr-CkgPQaKg/edit?usp=sharing"
+                  target="_blank"
+                >
                   {" "}
                   here.
                 </a>
@@ -76,20 +81,20 @@ const MommafungModal = () => {
         </Row>
         <Row>
           <Col span={22} offset={1}>
-            <h2>Who are the users (people who will visite the webpage)?</h2>
+            <h2>Who are the users (people who will visit the website)?</h2>
           </Col>
         </Row>
         <Row className="section-content">
           <Col span={20} offset={2}>
             <p>
-              In order to better understand our user, we interviewed the
-              customers of MommaFung and summarized the interview results into
-              three personals. While we originally expected that our users will
-              be limited to young adults who love sharing food picture on the
-              social media, during the interview, we realized that many people
-              are actually also attracted by the Steam that is produced when
-              buns are cooked, and they enjoy watching the process that the bun
-              is made. Some people also simply enjoy the taste because it is so
+              In order to better understand the users, we interviewed
+              MommaFung's customers and categorized our users with three
+              personas. While we originally expected that our users will be
+              limited to young adults who love sharing food picture on social
+              media, we later learned from the interviews that many people are
+              actually attracted by the steam that is produced when buns are
+              cooked, and they enjoy watching the making process of the buns.
+              Some people also simply enjoy the taste because it is so
               delicious.
             </p>
             <div className="persona flex-container">
@@ -105,13 +110,16 @@ const MommafungModal = () => {
             </div>
             <p>
               We also summarized user scenarios and use cases for the three
-              kinds of users listed above, and developed a Features &
-              Functionality Table to cover those user needs.
+              personas listed above, and developed a Feature & Functionality
+              Table to cover those user needs.
             </p>
             <p>
               <i>
                 Details can be accessed through
-                <a href="https://docs.google.com/document/d/1zopXHcXaYxlprxHJv8hkuImWKTQcA-YrNZpTwvUxRDM/edit?usp=sharing">
+                <a
+                  href="https://docs.google.com/document/d/1zopXHcXaYxlprxHJv8hkuImWKTQcA-YrNZpTwvUxRDM/edit?usp=sharing"
+                  target="_blank"
+                >
                   {" "}
                   here.
                 </a>
@@ -128,9 +136,12 @@ const MommafungModal = () => {
           <Col span={20} offset={2}>
             <p>
               We conducted a close analysis of each of the competitor website
-              from 6 different aspects: branding, functionality, content, site
-              architecture, navigation, and design. Then, we summarized the
-              fundings and made a list of takeaways for our website.
+              from 6 different aspects:{" "}
+              <strong>
+                Branding, Functionality, Content, Site Architecture, Navigation,
+              </strong>{" "}
+              and <strong>Design</strong>. Then, we summarized the findings and
+              made a list of takeaways for MommaFung's website.
             </p>
 
             <p>
@@ -147,8 +158,8 @@ const MommafungModal = () => {
                     social media links, and press sections.
                   </li>
                   <li>
-                    The address will link to google map navigation, which allows
-                    user to easily find the way to access the place
+                    Clicking on the address will open the google map, which
+                    saves users' time of copying and pasting
                   </li>
                   <li>
                     The text and section buttons are big and easy to click,
@@ -159,7 +170,7 @@ const MommafungModal = () => {
             </Row>
 
             <p>
-              Another example is Rainbow Bagel. Their website is conveying their
+              Another example is Rainbow Bagel. Their website is conveying the
               brand very wisely:
             </p>
             <Row className="example-block">
@@ -169,11 +180,11 @@ const MommafungModal = () => {
               <Col span={16} offset={4}>
                 <ul>
                   <li>
-                    Rainbow Bagel included their making process as a video. This
-                    demonstrates the hard work and effort put into the food,
-                    providing an insight into the people behind the product and
-                    allowing the customers to connect on a deeper level with the
-                    brand, the people, and the food.
+                    Rainbow Bagel includes a video of the making process of the
+                    bagels, which demonstrates the hard work and effort put into
+                    the food and provides an insight into the people behind the
+                    product. It allows the customers to connect on a deeper
+                    level with Rainbow Bagel's brand, the people, and the food.
                   </li>
                 </ul>
               </Col>
@@ -181,13 +192,12 @@ const MommafungModal = () => {
 
             <p>
               Through the process of analyzing the competitor websites, we
-              developed a sense of what are the “Goods” and “Bads” that are
-              specific to the vendor food industry. For example, food vendors
-              are different from the traditional restaurants that they do not
-              have much dish variety in menu, but they have random opening hours
-              and pop up locations. With the uniqueness of the vendor’s business
-              style in mind, we developed a Moodboard that will fit MommaFung’s
-              brand.
+              developed a sense of what are the “Goods” and “Bads” for a vendor
+              website. For example, food vendors are different from the
+              traditional restaurants that they do not have much dish variety in
+              menu, but they have random opening hours and pop up locations.
+              With the uniqueness of the vendor’s business style in mind, we
+              developed a mood board that will fit MommaFung’s brand.
             </p>
             <Row className="example-block">
               <Col span={18} offset={3}>
@@ -199,7 +209,10 @@ const MommafungModal = () => {
               <i>
                 What to see check more details? Our analysis details can be
                 accessed through
-                <a href="https://docs.google.com/presentation/d/1sjPY7IY2sTaBJA0ragfKe_ckGY2-O053beE2ND5dir4/edit?usp=sharing">
+                <a
+                  href="https://docs.google.com/presentation/d/1sjPY7IY2sTaBJA0ragfKe_ckGY2-O053beE2ND5dir4/edit?usp=sharing"
+                  target="_blank"
+                >
                   {" "}
                   here
                 </a>
@@ -211,8 +224,8 @@ const MommafungModal = () => {
         <Row>
           <Col span={22} offset={1}>
             <h2>
-              Now we have the higher level image. Let’s get into details on how
-              we will tackle this.
+              With all the analysis results, let’s get into details on how we
+              will tackle this.
             </h2>
           </Col>
         </Row>
@@ -227,23 +240,23 @@ const MommafungModal = () => {
               <li>
                 Scope
                 <ul>
-                  <li>Page contents</li>
-                  <li>Total program size</li>
+                  <li>Page Contents</li>
+                  <li>Total Program Size</li>
                 </ul>
               </li>
               <li>
                 Audience
                 <ul>
-                  <li>Returning customer</li>
-                  <li>Potential customer</li>
+                  <li>Returning Customer</li>
+                  <li>Potential Customer</li>
                   <li>Photographers, Instagrammers, Foodies</li>
                 </ul>
               </li>
               <li>
                 Objectives
                 <ul>
-                  <li>MommaFung's objective</li>
-                  <li>Audience's objective</li>
+                  <li>MommaFung's Objective</li>
+                  <li>Audience's Objective</li>
                 </ul>
               </li>
               <li>Personality and Tone</li>
@@ -255,7 +268,10 @@ const MommafungModal = () => {
                 This is a very long document, and most of them are the
                 repetition of what I have mentioned above. If you are interested
                 in reading more, click
-                <a href="https://docs.google.com/document/d/1hTselc6Zko0F0t4M1B1ZBHmVyeJaNvK3bZKemiOJvbk/edit?usp=sharing">
+                <a
+                  href="https://docs.google.com/document/d/1hTselc6Zko0F0t4M1B1ZBHmVyeJaNvK3bZKemiOJvbk/edit?usp=sharing"
+                  target="_blank"
+                >
                   {" "}
                   here.
                 </a>
@@ -272,11 +288,17 @@ const MommafungModal = () => {
           <Col span={20} offset={2}>
             <p>
               Based on our creative brief, we made a Development plan,{" "}
-              <a href="https://www.figma.com/proto/Vi0rT7eSTMWS0QST3GDqfy3q/Momma-Fung-Wireframes?scaling=contain&node-id=1%3A2">
+              <a
+                href="https://www.figma.com/proto/Vi0rT7eSTMWS0QST3GDqfy3q/Momma-Fung-Wireframes?scaling=contain&node-id=1%3A2"
+                target="_blank"
+              >
                 MOBILE Wireframe
               </a>{" "}
               and{" "}
-              <a href="https://www.figma.com/proto/Vi0rT7eSTMWS0QST3GDqfy3q/Momma-Fung-Wireframes?node-id=54%3A295&scaling=contain">
+              <a
+                href="https://www.figma.com/proto/Vi0rT7eSTMWS0QST3GDqfy3q/Momma-Fung-Wireframes?node-id=54%3A295&scaling=contain"
+                target="_blank"
+              >
                 DESKTOP Wireframe
               </a>
               .
